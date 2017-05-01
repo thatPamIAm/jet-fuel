1b3cb1afa58eedb83d55e30d12d330c6d7fdf117
Hugh Morrison <hmorri32@gmail.com>
2017-05-01 17:44:03 -0600

    npm i md5 and body parser

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app     = express();
-const path = require('path');
+const path    = require('path');
 
 
 app.use(express.static('public'))
 
-app.set('port', 3000)
-
+app.set('port', process.env.PORT || 3000)
 app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.locals.folders = []
+app.locals.urls    = []
+
 
 app.get('/', (request, response) => {
   response.send('index.html')
