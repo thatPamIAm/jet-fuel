4f6505ef20f3be366910450c8f6411db8fb45341
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-05-04 15:47:22 -0600

    updated post for urls

--- server.js
+++ server.js
@@ -80,9 +80,8 @@ app.get('/api/v1/urls', (request, response) => {
 app.post('/api/v1/urls', (request, response) => {
   const url = request.body
 
-  database('urls').insert(url ,[ "url_name","folder_id","long_url", "id"])
+  database('urls').insert(url ,[ "url_name","folder_id","long_url", "id", "visit_count"])
     .then((urls) => {
-      console.log(urls)
       response.status(201).json(urls[0])
     })
 })
