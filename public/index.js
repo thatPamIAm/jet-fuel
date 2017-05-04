b73f57479e30a946a475f7851012bc3f2ce942ca
Hugh Morrison <hmorri32@gmail.com>
2017-05-03 20:51:05 -0600

    implement sass, restructure index.js

--- public/index.js
+++ public/index.js
@@ -1,19 +1,36 @@
-$('document').ready(
+const fetchFolders = () => {
   fetch('/api/v1/folders')
   .then(response => response.json())
   .then(json => {
     json.map((folder) => {
-      appendFolder(folder.title)
+      appendFolder(folder.folder_name)
       console.log(folder);
     })
   })
-)
+  .catch(e => console.log(e))
+}
+
+$('document').ready(fetchFolders())
+
+const clearInput = (input) => {
+  $(`${input}`).val('')
+}
+
+const toggleFolderButton = (e) => {
+  let b = $('.create-folder-btn')
+  e.target.value ? b.attr('disabled', false) : b.attr('disabled', true)
+}
+
+$('.create-folder-input').on('keyup', (e) => {
+  toggleFolderButton(e)
+})
 
 $('.create-folder-btn').on('click', (e) => {
   e.preventDefault()
   let input = $('.create-folder-input').val()
   postFolder(input)
   appendFolder(input)
+  clearInput('.create-folder-input')
 })
 
 const postFolder = (input) => {
@@ -35,14 +52,29 @@ const appendFolder = (input) => {
   `)
 }
 
-
 // URL STUFF
+const validateUrl = (url) => {
+  var res = url.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)
+  if(res ) {
+    return true;
+  }
+  else {
+    return false
+  }
+}
+
+const addHTTP = (url) => {
+  if(!/^(f|ht)tps?:\/\//i.test(url)) {
+    url = "http://" + url;
+  }
+  return url;
+}
 
 $('.create-url-btn').on('click', (e) => {
   e.preventDefault()
 
   let name = $('.site-name-input').val()
-  let url = $('.url-input').val()
+  let url  = $('.url-input').val()
   if(validateUrl(url) == false) {
     console.log('false');
   }
@@ -52,7 +84,7 @@ $('.create-url-btn').on('click', (e) => {
   }
 })
 
-function postURL(name, url) {
+const postURL = (name, url) => {
   fetch('/api/v1/urls', {
     method: 'POST',
     headers: {
@@ -68,22 +100,6 @@ function postURL(name, url) {
   .catch(e => console.log(e))
 }
 
-function addHTTP(url) {
-  if(!/^(f|ht)tps?:\/\//i.test(url)) {
-    url = "http://" + url;
-  }
-  return url;
-}
-
-function validateUrl(url) {
-  var res = url.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)
-  if(res ) {
-    return true;
-  }
-  else {
-    return false
-  }
-}
 
 
 
