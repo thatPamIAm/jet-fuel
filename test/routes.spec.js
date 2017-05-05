77977fe96d4f6e85c40a45f42554650564408a4e
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-05-05 09:19:03 -0600

    cool tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -53,51 +53,59 @@ describe('server side testing', () => {
 
   describe('API Routes', () => {
 
+    describe('GET /api/v1/folders', (request, response) => {
+      it('should return all of the folders', (done) => {
+        chai.request(server)
+        .get('/api/v1/folders')
+        .end((err, response) => {
+          response.should.have.status(200)
+          response.should.be.json
+          response.body.should.be.a('array')
+          response.body.length.should.equal(2)
+          response.body[0].should.have.property('folder_name')
+          response.body[0].folder_name.should.equal('really cool folder')
+          response.body[1].should.have.property('folder_name')
+          response.body[1].folder_name.should.equal('another chic folder')
 
+          done()
+        })
+      })
+    })
 
+    describe('GET /api/v1/folders/:id', (request, response) => {
+      it.only('should return a single folder by an ID', (done) => {
+        chai.request(server)
+        .get('/api/v1/folders/1')
+        .end((err, response) => {
+          console.log(err, response.body);
+          response.should.have.status(200)
+          response.should.be.json
+          response.body.should.be.a('array')
+          response.body.length.should.equal(1)
+          response.body[0].should.have.property('folder_name')
+          response.body[0].folder_name.should.equal('really cool folder')
 
+          done()
+        })
+      })
+    })
 
+    describe('GET /api/v1/folders/:id/urls', (request, response) => {
+      it.only('should return all the URLs from a single folder', (done) => {
+        chai.request(server)
+        .get('/api/v1/folders/1/urls')
+        .end((err, response) => {
+          response.should.have.status(200)
+          response.should.be.json
+          response.body.should.be.a('array')
+          response.body.length.should.equal(2)
+          response.body[0].should.have.property('url_name')
+          response.body[0].url_name.should.equal(' testing wbesites')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          done()
+        })
+      })
+    })
 
     describe('POST /api/v1/folders', () => {
       it('should allow me to create a new folder', (done) => {
