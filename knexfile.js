56304633aa89a75de4889c71710db7567e2a07af
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-05-08 10:06:36 -0600

    add staging environment

--- knexfile.js
+++ knexfile.js
@@ -26,6 +26,18 @@ module.exports = {
     useNullAsDefault: true
   },
 
+  staging: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
